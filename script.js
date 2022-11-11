c04899f7cb9db1add1cf672b351033e0f97c2f6e
Emmanuel Norambuena <eanorambuena@uc.cl>
2022-11-11 15:19:26 -0300

    Complete review-1

--- script.js
+++ script.js
@@ -15,6 +15,11 @@ const addContact = (
 }
 
 const removeContact = (contactList, contactFirstName, contactLastName) => {
+    const contactIndex = foundContact(contactList, contactFirstName, contactLastName);
+    return contactList.splice(contactIndex, 1);
+}
+
+const foundContact = (contactList, contactFirstName, contactLastName) => {
     let resultIndex = -1;
     for (let index = 0; index < contactList.length; index ++) {
         if (contactList[index].nombres == contactFirstName
@@ -22,10 +27,24 @@ const removeContact = (contactList, contactFirstName, contactLastName) => {
             resultIndex = index;
         }
     }
-    return contactList.splice(resultIndex, 1);
+    return resultIndex;
 }
 
-const printContacts = (contactList) => {
+function printContacts(contactList) {
     contactString = contactList.join(", ");
     console.log(contactString);
+}
+
+const updateContact = (contactList, contactFirstName, contactLastName, replacementObject) => {
+    const contactIndex = foundContact(contactList, contactFirstName, contactLastName);
+    const replacementEntries = Object.entries(replacementObject);
+    let key;
+    let value;
+    let replacement;
+    for (let index = 0; index < replacementEntries.length; index++) {
+        replacement = replacementEntries[index];
+        key = replacement[0];
+        value = replacement[1];
+        contactList[contactIndex][key] = value;
+    }
 }
